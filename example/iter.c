ee7a049380c4b02ebc7f49492869c8cd78be6d30
joel <bin3340@163.com>
2021-11-15 23:13:32 +0800

    Update iter.c

--- example/iter.c
+++ example/iter.c
@@ -30,92 +30,9 @@
 
 #include "serializer.h"
 #include "ctdb.h"
+#include "utils.h"
 
-///////////////////////////////////////////////////////////////////////////////
-// UTILS
-///////////////////////////////////////////////////////////////////////////////
-static int random_range(int min, int max) __attribute__((unused));
-static int random_range(int min, int max){
-    return rand() % (max - min + 1) + min;
-}
-
-static int64_t getCurrentTime() __attribute__((unused));
-static int64_t getCurrentTime() {
-   struct timeval tv;
-   gettimeofday(&tv,NULL);
-   return tv.tv_sec * 1000 + tv.tv_usec / 1000;
-}
-
-static char *random_str(int len) __attribute__((unused));
-static char *random_str(int len) {
-    int i;
-    char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    char *string = malloc(len + 1);
-    if(NULL == string) return NULL;
-    for (i = 0; i < len; i++) {
-        string[i] = alphabet[rand() % 62];
-    }
-    string[i] = 0;
-    return string;
-}
-
-static char *random_str_shortly(int len) __attribute__((unused));
-static char *random_str_shortly(int len) {
-    int i;
-    char *alphabet = "abcdefghijklmnopqrstuvwxyz0123456789._";
-    char *string = malloc(len + 1);
-    if(NULL == string) return NULL;
-    for (i = 0; i < len; i++) {
-        string[i] = alphabet[rand() % 38];
-    }
-    string[i] = 0;
-    return string;
-}
-
-static char *random_str_num(int len) __attribute__((unused));
-static char *random_str_num(int len) {
-    int i;
-    char *alphabet = "0123456789";
-    char *string = malloc(len + 1);
-    if(NULL == string) return NULL;
-    for (i = 0; i < len; i++) {
-        string[i] = alphabet[rand() % 10];
-    }
-    string[i] = 0;
-    return string;
-}
-
-static char *read_value_from_file(int fd, uint32_t value_len, off_t value_pos) __attribute__((unused));
-static char *read_value_from_file(int fd, uint32_t value_len, off_t value_pos) {
-    char *value = calloc(1, value_len);
-    if (NULL == value) goto err;
-    if (-1 == lseek(fd, value_pos, SEEK_SET)) goto err;
-    if (value_len != read(fd, value, value_len)) goto err;
-    return value;
-
-err:
-    if (NULL != value) free(value);
-    return NULL;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// TESTING
-///////////////////////////////////////////////////////////////////////////////
 int g_iter_count = 0;
-#if defined(__APPLE__)
-    int traversal(int fd, char *key, uint8_t key_len, struct ctdb_leaf leaf){
-        g_iter_count += 1;
-
-        assert(0 < leaf.value_len);
-        char *value = read_value_from_file(fd, leaf.value_len, leaf.value_pos);
-        assert(NULL != value);
-        printf("key:%.*s value:%.*s\n", key_len, key, leaf.value_len, value);
-        assert(key_len == leaf.value_len && 0 == strncmp(key, value, key_len));
-        free(value);
-
-        return CTDB_OK; //continue
-    }
-#endif
 void test_iter(int count, char *prefix, uint8_t prefix_len) {
     int key_len = 32;
     char *path = "./test.db";
@@ -137,34 +54,29 @@ void test_iter(int count, char *prefix, uint8_t prefix_len) {
     }
 
     assert(CTDB_OK == ctdb_transaction_commit(trans));
-    ctdb_transaction_free(trans);
+    ctdb_transaction_free(&trans);
     
-
     assert(NULL != (trans = ctdb_transaction_begin(db)));
     g_iter_count = 0;
-#if defined(__APPLE__)
-    if(CTDB_OK == ctdb_iterator_travel(trans, prefix, prefix_len, traversal)){
-        printf("iterator sucess, prefix:'%s' count:%llu iter_count:%d\n", prefix, trans->footer.tran_count, g_iter_count);
-    }
-#else
+    
     int traversal(int fd, char *key, uint8_t key_len, struct ctdb_leaf leaf){
         g_iter_count += 1;
-
         assert(0 < leaf.value_len);
+        assert(0 < leaf.value_pos);
         char *value = read_value_from_file(fd, leaf.value_len, leaf.value_pos);
-        assert(NULL != value);
         printf("key:%.*s value:%.*s\n", key_len, key, leaf.value_len, value);
         assert(key_len == leaf.value_len && 0 == strncmp(key, value, key_len));
         free(value);
-        
         return CTDB_OK; //continue
     }
     if(CTDB_OK == ctdb_iterator_travel(trans, prefix, prefix_len, traversal)){
-        printf("iterator sucess, prefix:'%s' count:%llu iter_count:%d\n", prefix, trans->footer.tran_count, g_iter_count);
+        printf("iterator sucess, prefix:'%s' count:%lu iter_count:%d\n", prefix, trans->footer.tran_count, g_iter_count);
+    }else{
+        printf("iterator failed!!!\n");
     }
-#endif
-    ctdb_transaction_free(trans);
-    ctdb_close(db);
+
+    ctdb_transaction_free(&trans);
+    ctdb_close(&db);
 }
 
 int main(){
